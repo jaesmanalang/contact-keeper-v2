807c8e48a7d5bec55a591a0b1cff47ba0e478b66
James Manalang <jaesmanalang@gmail.com>
2020-08-13 00:26:30 +0800

    Add landing page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { Fragment } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  BrowserRouter,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Header } from './components';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
     <Fragment>
       <Router>
         <Header />
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+        </Switch>
       </Router>
     </Fragment>
   );
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const LandingPage = () => (
+  <div className="welcome">
+    <h1>Welcome to Contact Keeper</h1>
+    <p>Keep. Organize. Manage.</p>
+  </div>
+);
+
+export default LandingPage;
